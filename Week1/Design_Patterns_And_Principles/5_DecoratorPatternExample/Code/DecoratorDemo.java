cf50b9d0c5284b3bdff56f39fbe0cde63f43448b
Suyash Kawathalkar <suyashkawathalkar@gmail.com>
2025-06-20 04:29:11 +0530

    Added DecoratorPatternExample Exercise

---
+++ Week1/Design_Patterns_And_Principles/5_DecoratorPatternExample/Code/DecoratorDemo.java
@@ -0,0 +1,11 @@
+public class DecoratorDemo {
+    public static void main(String[] args) {
+        Notifier notifier = new EmailNotifier();
+
+        notifier = new SMSNotifierDecorator(notifier);
+
+        notifier = new SlackNotifierDecorator(notifier);
+
+        notifier.send("Your order has been shipped!");
+    }
+}
---
+++ Week1/Design_Patterns_And_Principles/5_DecoratorPatternExample/Code/NotifierDecorator.java
@@ -0,0 +1,12 @@
+public abstract class NotifierDecorator implements Notifier {
+    protected final Notifier wrappee;
+
+    public NotifierDecorator(Notifier notifier) {
+        this.wrappee = notifier;
+    }
+
+    @Override
+    public void send(String message) {
+        wrappee.send(message);
+    }
+}
