8be8f088bbb26fdfbcb52af56e860feda2f33d57
Suyash Kawathalkar <suyashkawathalkar@gmail.com>
2025-07-04 14:59:44 +0530

    Completed Spring_data_JPA Quick Example

---
+++ Week3/2_Spring_Data_jpa/1_JPA_QuickExample/Code/orm-learn/src/test/java/com/cognizant/orm_learn/DemoProjectForSpringDataJpaAndHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cognizant.orm_learn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoProjectForSpringDataJpaAndHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
