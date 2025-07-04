a5c0b8ef1af0e63b9b7223a95aba61f7c013201d
Suyash-K <114049661+Suyash-K@users.noreply.github.com>
2025-07-04 15:04:02 +0530

    Small Bug FIx

--- Week2/JUnit/Code/4_Exercise/src/main/java/mainUtils.java
+++
@@ -1,9 +0,0 @@
-public class MathUtils {
-    public int multiply(int a, int b) {
-        return a * b;
-    }
-
-    public int divide(int a, int b) {
-        return a / b;  // throws ArithmeticException if b == 0
-    }
-}
